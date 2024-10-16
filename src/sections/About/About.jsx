0c07fa7fc8b7216df07ccf9a90d5f5d6e6a3123f
MandySpaan <mandyspaan94@gmail.com>
2024-10-16 10:58:07 +0200

    feat(About): Text and basic layout

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,33 +1,38 @@
+import "./About.css";
+
 const About = () => {
   return (
-    <div>
-      About <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+    <div className="about-section">
+      <h1>About</h1>
+      <div className="about-content">
+        <div className="about-text">
+          <h2>On my Developer Journey</h2>
+          <p>
+            I've explored a lot of different industries before landing on
+            software development, from television and marketing to hospitality
+            and more. I've always been someone who focuses on growth and
+            learning, and I'm excited to have finally found a field where the
+            limits for this seem endless.
+            <br />
+            <br />A year ago, I took my first real step into tech with an online
+            Frontend Developer course from Meta. Finishing that course only
+            fueled my curiosity further, leading me to enroll in a Full-Stack
+            Development bootcamp. The experience has been quite a whirlwind,
+            throwing me headfirst into the developer world.
+            <br />
+            <br />I won't sugarcoat it, there have been moments when I felt like
+            tossing my laptop (or myself) out the window. But the challenges
+            have been worth it! The progress I've made in just a few months,
+            often spending over 11 hours a day coding, has been immensely
+            rewarding. Now, I'm excited to apply my skills in a developer role
+            where I can continue to grow and learn in a field with endless
+            opportunities.
+          </p>
+        </div>
+        <div className="about-stack">
+          <h2>Technology Stack</h2>
+        </div>
+      </div>
     </div>
   );
 };

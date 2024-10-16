1b98c067745f8b9263e767085dba4dfd780a000d
MandySpaan <mandyspaan94@gmail.com>
2024-10-16 10:41:14 +0200

    refactor: General responsive design functionality in App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./sections/Home/Home";
+import "./App.css";
 
 const App = () => {
   return (
